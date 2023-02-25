d36c452bd3b2e49a6d2c976ddd9121882b8a0470
Aderain <aydarisaev@mail.ru>
2023-02-25 19:48:08 +0600

    navbar fix when clicked on link

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 $(document).ready(() => {
     $(".menu-bar").click(() => {
         $(".nav-menu").removeClass("hidden");
+
+        $(".nav-list_link").click(() => {
+            $(".nav-menu").addClass("hidden");
+        });
     })
    
     $(".menu-close").click(() => {
         $(".nav-menu").addClass("hidden");
     })
+
 })
