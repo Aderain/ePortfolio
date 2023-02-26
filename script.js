4c3e6132a089b912239947a2ba1dc37ae53c298f
Aderain <aydarisaev@mail.ru>
2023-02-26 12:50:59 +0600

    small fixes

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 $(document).ready(() => {
     $(".menu-bar").click(() => {
         $(".nav-menu").removeClass("hidden");
-
-        $(".nav-list_link").click(() => {
-            $(".nav-menu").addClass("hidden");
-        });
     })
    
     $(".menu-close").click(() => {
         $(".nav-menu").addClass("hidden");
     })
 
+    $(".nav-menu .nav-list_link").each((i, value) => {
+        $(value).click(() => {
+            // $(".nav-menu .nav-list_link").each((i, val) => {
+            //     $(val).removeClass("active");
+            // });
+            $(".nav-menu").addClass("hidden");
+            // $(value).addClass("active");
+        });
+    })
+
+    $(".nav-list .nav-list_link").each((i, value) => {
+        $(value).click(() => {
+            $(".nav-list .nav-list_link").each((i, val) => {
+                $(val).removeClass("active");
+            });
+            $(value).addClass("active");
+        });
+    })
 })
+
+const section = document.querySelectorAll("section");
+const links = document.querySelectorAll(".nav-list .nav-list_link");
+
+document.addEventListener("scroll", () => {
+    section.forEach((el, i) => {
+        if(el.getBoundingClientRect().top <= 0){        
+            
+            links.forEach((el, j) =>{
+                el.classList.remove("active");
+            })
+            
+            links[i].classList.add("active");
+        
+        }
+    })
+});
+
